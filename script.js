b010e04e6808aec043fb3a13ea0a692ac4712b59
owentr1369 <tamvaa1306@gmail.com>
2022-06-06 14:26:32 +0700

    fetching link functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+function getRandomMeal() {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+}
+function getMealById(id) {
+  fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+}
+function getMealsBySearch(term) {
+  fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${tern}`);
+}
